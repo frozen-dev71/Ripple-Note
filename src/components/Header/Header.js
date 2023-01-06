83ab8e299348aea6b36eefbbc4f14b52f607647c
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-06 22:09:38 +0300

    Created Header Component and imported required images

---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import logo from "../../assets/images/noteapp.png";
+import menu from "../../assets/images/xboxmenu.png";
+import close from "../../assets/images/cancel.png";
+import userImg from "../../assets/images/user.png";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
