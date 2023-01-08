9b474822e7719bc871587c9e3746c122050ee140
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-08 17:08:41 +0300

    Added mockupdata and usermockup data to load user notes from firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import {Main} from './components/Main/Main'
+import {Main} from './components/Main/Main';
+import { useState, useEffect } from "react";
+
 
 function App() {
   return (
     <div className="App">
       <Main />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
