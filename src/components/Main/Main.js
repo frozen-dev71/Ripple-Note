dd9a247e09c6f56ed517358ca2c1c5dc6a6c3423
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-08 16:52:51 +0300

    added mouse move functionality

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,3 +5,23 @@ import { Link } from "react-router-dom";
 import ScrollToTop from "../../helpers/ScrollToTop";
 import Loader from "../Loader";
 
+const Main = ({
+	user,
+	logout,
+	currentUserFromDb,
+	waitForUserFromDb,
+	currentPage,
+	globalCoords,
+	handleMouseMove,
+	coords,
+  }) => {
+	
+	let style = {
+		top: `${globalCoords.y - 100}px`,
+		left: `${globalCoords.x - 100}px`,
+	  };
+
+	return(
+
+	);
+};
