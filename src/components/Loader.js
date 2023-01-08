b948393ebaff646c14313451713ebe4369c6abf4
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-08 16:47:23 +0300

    Added Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+const Loader = () => {
+	return (
+	  <div className="w-full h-[100vh] fixed top-0 left-0 bg-black/90 flex justify-center items-center z-50">
+		<div className="bg-[#252525] border border-teal-400 text-white text-[1.2rem] md:text-[2rem] font-bold w-[80%] md:w-[40%] py-6 md:py-12 rounded-lg flex justify-center items-center">
+		  Loading...
+		</div>
+	  </div>
+	);
+  };
+  
+  export default Loader;
+  
