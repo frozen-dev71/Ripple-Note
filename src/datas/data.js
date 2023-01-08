9f73d6d87cee35aa429ba31b1cfe469b22d969e4
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-08 17:01:08 +0300

    created mockup datas to display

---
+++ src/datas/data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+      id: 1,  
+      title: "Steps to take on how to look, smile, code and live like Berk",
+      date: "jan 05, 2023",
+      body: "Blah blah.. make your own note :) /n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      hover: false
+    },
+    {
+      id: 2,  
+      title: "Finibus Bonorum et Malorum",
+      date: "june 16, 2023",
+      body: "Blah blah.. make your own note. \n At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      hover: false
+    },
+    {
+      id: 3,  
+      title: "How to make your brand stand out online",
+      date: "sept 2, 2023",
+      body: "Blah blah.. make your own note. \n Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      hover: false
+    },
+    {
+      id: 4,  
+      title: "How to code like Berk",
+      date: "may 16, 2023",
+      body: "Blah blah.. make your own note.",
+      hover: false
+    },
+    {
+      id: 5,  
+      title: "10 great React coding practices",
+      date: "mar 15, 2023",
+      body: "Blah blah.. make your own note.",
+      hover: false
+    },
+]
+
+export default data;
