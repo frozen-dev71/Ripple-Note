af94012457cd5fc68ab1192e1045aec3683e6e2e
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-08 16:51:40 +0300

    İmported required objects for the Main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import {Main} from './components/Main/Main'
 
 function App() {
   return (
     <div className="App">
+      <Main />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Main/Main.js
@@ -0,0 +1,7 @@
+import hero from "../../assets/images/searching-icon.svg";
+import crown from "./images/handy-line.png";
+import Header from "../Header/Header";
+import { Link } from "react-router-dom";
+import ScrollToTop from "../../helpers/ScrollToTop";
+import Loader from "../Loader";
+
