33ae11b5a6502809d1bd4fa54d1ed8fa602a6ea5
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-08 17:11:57 +0300

    added firebase logs and added cursor movement tracker

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import {Main} from './components/Main/Main';
 import { useState, useEffect } from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 function App() {
+  const location = useLocation();
+  let currentPage = location.pathname;
+
+  //to track cursour movement
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+
   return (
     <div className="App">
       <Main />
