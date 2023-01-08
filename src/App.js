e9b8b6566fd46384879ef2bd4493e18b7c435b70
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-08 17:13:15 +0300

    Added function for tracking mouse global movement

--- src/App.js
+++ src/App.js
@@ -16,6 +16,32 @@ function App() {
   //to track cursour movement
   const [coords, setCoords] = useState({ x: 0, y: 0 });
 
+  const [globalCoords, setGlobalCoords] = useState({ x: 0, y: 0 });
+
+
+  const handleMouseMove = (event) => {
+    setCoords({
+      x: event.clientX - event.target.offsetLeft,
+      y: event.clientY - event.target.offsetTop,
+    });
+  };
+
+  useEffect(() => {
+    // 👇️ get global mouse coordinates
+    const handleWindowMouseMove = (event) => {
+      setGlobalCoords({
+        x: event.screenX,
+        y: event.screenY,
+      });
+    };
+    window.addEventListener("mousemove", handleWindowMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleWindowMouseMove);
+    };
+  }, []);
+
+  
 
   return (
     <div className="App">
